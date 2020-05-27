3fcbd205d70f9f7a6bf9de72804653e2616efe94
Jorge Souza <regi.jorge.s@gmail.com>
2020-05-26 22:23:47 -0300

    feat: add post schema and resolver

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -4,6 +4,7 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
     type Query {
         me: User
+        post: Post
     }
 
     type User {
@@ -11,6 +12,13 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `
 
@@ -23,6 +31,14 @@ const resolvers = {
                 name: 'Jorge',
                 email: 'test@gmail.com',
                 age: 27
+            }
+        },
+        post() {
+            return {
+                id: '321',
+                title: 'My Blog Post',
+                body: 'My blog post content',
+                published: true
             }
         }
     }
