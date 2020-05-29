a41cd7fd8b5a04ad697cbc68accab60fe70af584
Jorge Souza <regi.jorge.s@gmail.com>
2020-05-29 13:10:02 -0300

    feat: add object spread operator

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -156,9 +156,7 @@ const resolvers = {
 
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
             }
 
             users.push(user)
@@ -174,10 +172,7 @@ const resolvers = {
 
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args
             }
 
             posts.push(post)
@@ -194,9 +189,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args
             }
 
             comments.push(comment)
